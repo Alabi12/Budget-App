20149c3ba5305390eb6afb43512723ecc7f5bc39
Alabi12 <wolo22001@gmail.com>
2023-03-28 22:23:55 +0000

    Update the model files

--- app/models/expenditure.rb
+++ app/models/expenditure.rb
@@ -1,4 +1,7 @@
 class Expenditure < ApplicationRecord
-  belongs_to :user
   has_and_belongs_to_many :categories
+  belongs_to :author, class_name: "User"
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
