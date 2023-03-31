f327c91ab9637001ce7872cbd875068bc6823dbd
Alabi12 <wolo22001@gmail.com>
2023-03-31 19:09:30 +0000

    Add testing files

--- spec/models/expenditure_spec.rb
+++ spec/models/expenditure_spec.rb
@@ -1,14 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe Expenditure, type: :model do
-  context 'validation tests' do
-    it 'ensure name presence' do
-      expenditure = Expenditure.new(name: 'John')
-      expect(expenditure.name.present?).to eq(true)
-    end
-    it 'ensure amount presence' do
-      expenditure = Expenditure.new(name: 'John', amount: 100)
-      expect(expenditure.amount.present?).to eq(true)
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context 'validation tests' do
-    it 'ensures name and email presence' do
-      user = User.new(name: 'alabi', email: 'wolo22001@gmail.com').save
-      expect(user).to eq(false)
-    end
-    it 'ensure name presence' do
-      user = User.new(name: 'John')
-      expect(user.name.present?).to eq(true)
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
